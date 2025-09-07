e0635318ecfc650885741f10dac2c0b4e6e43fea
Micker <carbonellerick811@gmail.com>
2025-09-07 00:48:43 -0600

    Add: Comentados los DROP TABLE para no borrar todo cada vez

--- backend/backendNuevo/server.js
+++ backend/backendNuevo/server.js
@@ -15,10 +15,10 @@ const db = new sqlite3.Database('./users.db', err => {
 
 // 2. Crea la tabla usuarios y un registro de ejemplo
 db.serialize(() => {
-  // Elimiar tablas anteriores
-  db.run("DROP TABLE IF EXISTS users_cursos");
-  db.run("DROP TABLE IF EXISTS cursos");
-  db.run("DROP TABLE IF EXISTS users");
+  // Elimiar tablas anteriores comantadas para por si acaso
+  // db.run("DROP TABLE IF EXISTS users_cursos");
+  // db.run("DROP TABLE IF EXISTS cursos");
+  // db.run("DROP TABLE IF EXISTS users");
 
 
   // Tabla de usuarios
