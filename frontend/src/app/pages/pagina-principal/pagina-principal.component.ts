b8d04b6c01583892a95efe543f1efe9c56464c56
Leonardo Salvador <Willileo.salvadorwilliam872@gmail.com>
2025-09-09 00:56:38 -0600

    Se los filtros para la busqueda de publicaciones

--- frontend/src/app/pages/pagina-principal/pagina-principal.component.ts
+++ frontend/src/app/pages/pagina-principal/pagina-principal.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-principal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './pagina-principal.component.html',
   styleUrls: ['./pagina-principal.component.css']
 })
 export class PaginaPrincipalComponent implements OnInit {
-  posts: any[] = [];
+
+  // estos son valores de prueba para los posts
+  // posts: any[] = []; este es el original
+  posts = [
+    { curso: 'Matematica I', catedratico: 'Juan Pérez'},
+    { curso: 'Fisica II', catedratico: 'María López'},
+    { curso: 'Programacion I', catedratico: 'Carlos Gómez'},
+    { curso: 'Lenguajes Formales', catedratico: 'Ana Torres'},
+    { curso: 'Lenguajes Formales', catedratico: 'Judith Najera'}
+  ];
+
+  filtroCurso = '';
+  filtroCatedratico = '';
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -27,10 +40,23 @@ export class PaginaPrincipalComponent implements OnInit {
     // en caso de haber sesion se pasa a la pagina principal
     this.http.get<any[]>('http://localhost:3000/api/posts')
       .subscribe({
-        next: data => this.posts = data,
+        next: data => {
+        if (data && data.length > 0) {
+          this.posts = data;
+        }
+        },
         error: () => console.error('Error al cargar publicaciones')
       });     
   }
+
+  //este metodo sera para realizar las busquedas de los cursos segun el filtro indicado
+    get publicacionesFiltradas() {
+    return this.posts.filter(pub =>
+      pub.curso.toLowerCase().includes(this.filtroCurso.toLowerCase()) &&
+      pub.catedratico.toLowerCase().includes(this.filtroCatedratico.toLowerCase())
+    );
+  }
+
 
   logout(): void {
     localStorage.removeItem('user'); 
